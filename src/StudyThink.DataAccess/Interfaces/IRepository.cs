59b8173e5e600d0efa74cef4b4c57cf8460226c4
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-03 18:13:11 +0500

    Repository Created

---
+++ src/StudyThink.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyThink.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        ValueTask<long> CountAsync();
+        ValueTask<T> GetByIdAsync(long Id);
+        ValueTask<IEnumerable<T>> GetAllAsync();
+        ValueTask<bool> Delete(int Id);
+        ValueTask<bool> Update(T model);
+        ValueTask<bool> Create(T model);
+    }
+}
