53d49dd39370ccd53c6204f8f64b7970f730cd56
Dilmurod3460 <dilmuroderkinov06@gmail.com>
2023-11-03 18:29:01 +0500

    Collobrates added

---
+++ src/StudyThink.Service/DTOs/Callaborators/CallaboratorsCreationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyThink.Service.DTOs.CallaboratorsDTO;
+
+public class CallaboratorsCreationDto
+{
+    public string Name { get; set; }
+    public string ImagePath { get; set; }
+    public string Description { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
