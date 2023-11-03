9f8ccdf12be39d9facb165c7f2dc1cedd2ea7759
Dilmurod3460 <dilmuroderkinov06@gmail.com>
2023-11-03 18:28:52 +0500

    Course Required added

--- src/StudyThink.Domain/Entities/Callaborators/CourseRequirment.cs
+++ src/StudyThink.Domain/Entities/Callaborators/CourseRequirment.cs
@@ -1,6 +1,6 @@
 ﻿namespace StudyThink.Domain.Entities.Callaborators;
 
-public class CourseRequirments : Auditable
+public class CourseRequirment : Auditable
 {
     public int CourseId { get; set; }
     public string Requirments { get; set; }
