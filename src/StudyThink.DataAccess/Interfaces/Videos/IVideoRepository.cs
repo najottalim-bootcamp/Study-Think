f0c18b18eb0a58dbf089fa687d5b342f8e063ecd
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-04 17:11:03 +0500

    improve

--- src/StudyThink.DataAccess/Interfaces/Videos/IVideoRepository.cs
+++ src/StudyThink.DataAccess/Interfaces/Videos/IVideoRepository.cs
@@ -1,10 +1,9 @@
 ﻿using StudyThink.DataAccess.Common;
 using StudyThink.Domain.Entities.Videos;
 
-namespace StudyThink.DataAccess.Interfaces.Videos
+namespace StudyThink.DataAccess.Interfaces.Videos;
+
+public interface IVideoRepository : IRepository<Video> , IGetAll<Video>
 {
-    public interface IVideoRepository : IRepository<Video> , IGetAll<Video>
-    {
-        ValueTask<IEnumerable<Video>> GetVideoByModulIdAsync(long modulId);
-    }
+    ValueTask<IEnumerable<Video>> GetVideoByModulIdAsync(long modulId);
 }
