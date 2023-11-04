db218a0f271406b6e656c6c698c91427960022c1
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-04 20:42:09 +0500

    added AdminRepository

---
+++ src/StudyThink.DataAccess/Repositories/Admins/AdminRepository.cs
@@ -0,0 +1,53 @@
+﻿using StudyThink.DataAccess.Interfaces.Admins;
+using StudyThink.DataAccess.Utils;
+using StudyThink.Domain.Entities.Admins;
+
+namespace StudyThink.DataAccess.Repositories.Admins;
+
+public class AdminRepository : BaseRepository, IAdminRepository
+{
+    public ValueTask<long> CountAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> CreateAsync(Admin model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DeleteAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<Admin>> GetAllAsync(PaginationParams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Admin> GetByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Admin> GetByIdAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<Admin>> GetByPhoneNumberAsync(string phoneNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<(long ItemsCount, IEnumerable<Admin>)> SearchAsync(string search, PaginationParams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> UpdateAsync(Admin model)
+    {
+        throw new NotImplementedException();
+    }
+}
