6356f80930ca852ecc29bfe4f22405a01375deb9
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-04 19:39:59 +0500

    interfaces improved

---
+++ src/StudyThink.DataAccess/Interfaces/Categories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using StudyThink.DataAccess.Common;
+using StudyThink.Domain.Entities.Categories;
+
+namespace StudyThink.DataAccess.Interfaces.Categories;
+
+public interface ICategoryRepository : IRepository<Category>,
+    IGetAll<Category>, ISearchable<Category>
+{
+}
