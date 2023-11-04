67b0ba4b1cd1f48c7653e3fd5d48bf417510e1b5
Dilmurod3460 <dilmuroderkinov06@gmail.com>
2023-11-04 17:55:32 +0500

    implement interface  and interfaces compleated

---
+++ src/StudyThink.DataAccess/Repositories/Callaborators/CallaboratorRepository.cs
@@ -0,0 +1,38 @@
+﻿using StudyThink.DataAccess.Interfaces.Coloborators;
+using StudyThink.DataAccess.Utils;
+using StudyThink.Domain.Entities.Callaborators;
+
+namespace StudyThink.DataAccess.Repositories.Callaborators;
+
+public class CallaboratorRepository : BaseRepository, ICalloboratorRepository
+{
+    public ValueTask<long> CountAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> CreateAsync(Callaborator model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DeleteAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<Callaborator>> GetAllAsync(PaginationParams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Callaborator> GetByIdAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> UpdateAsync(Callaborator model)
+    {
+        throw new NotImplementedException();
+    }
+}
