98e6dd0e1f4d33e4c9081e522b436527a404df59
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-04 20:33:18 +0500

    add categoryRepository

---
+++ src/StudyThink.DataAccess/Repositories/Categories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using StudyThink.DataAccess.Interfaces.Categories;
+using StudyThink.DataAccess.Utils;
+using StudyThink.Domain.Entities.Categories;
+
+namespace StudyThink.DataAccess.Repositories.Categories;
+
+public class CategoryRepository : BaseRepository, ICategoryRepository
+{
+    public ValueTask<long> CountAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> CreateAsync(Category model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DeleteAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<Category>> GetAllAsync(PaginationParams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Category> GetByIdAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<(long ItemsCount, IEnumerable<Category>)> SearchAsync(string search, PaginationParams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> UpdateAsync(Category model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateImageAsync(long categoryId, string imagePath)
+    {
+        throw new NotImplementedException();
+    }
+}
