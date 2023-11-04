85f121cb8dcb2e1e63d0d92097060fab0b96a254
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-04 12:08:09 +0500

    Repository Changed

--- src/StudyThink.DataAccess/Interfaces/IRepository.cs
+++ src/StudyThink.DataAccess/Interfaces/IRepository.cs
@@ -1,12 +1,10 @@
-﻿using StudyThink.DataAccess.Common;
-
-namespace StudyThink.DataAccess.Interfaces
+﻿namespace StudyThink.DataAccess.Interfaces
 {
-    public interface IRepository<T> where T : class, IGetAll<T>
+    public interface IRepository<T> where T : class
     {
         ValueTask<long> CountAsync();
         ValueTask<T> GetByIdAsync(long Id);
-        ValueTask<bool> DeleteAsync(int Id);
+        ValueTask<bool> DeleteAsync(long Id);
         ValueTask<bool> UpdateAsync(T model);
         ValueTask<bool> CreateAsync(T model);
 
