d1d06a04cedb7f8cd81c9aa3829c0beaef1ed20e
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-04 11:30:00 +0500

    added enums and Imrpove entities

--- src/StudyThink.Domain/Entities/Payments/Payment.cs
+++ src/StudyThink.Domain/Entities/Payments/Payment.cs
@@ -1,10 +1,12 @@
-﻿namespace StudyThink.Domain.Entities.Payments;
+﻿using StudyThink.Domain.Enums;
+
+namespace StudyThink.Domain.Entities.Payments;
 
 public class Payment : BaseEntity
 {
 
-    public string Type { get; set; }
-    public string Status { get; set; }
+    public PaymentType Type { get; set; }
+    public PaymentStatus Status { get; set; }
     public string Description { get; set; }
     public long CourseId { get; set; }
 }
