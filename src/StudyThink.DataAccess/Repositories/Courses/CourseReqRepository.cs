f82d396ce4e01015a95d32a17e832e4ec4c0234f
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-04 21:47:46 +0500

    added CourseReqRepository

---
+++ src/StudyThink.DataAccess/Repositories/Courses/CourseReqRepository.cs
@@ -0,0 +1,48 @@
+﻿using StudyThink.DataAccess.Utils;
+using StudyThink.Domain.Entities.Courses;
+using StudyThink.Service.Interfaces.Courses;
+
+namespace StudyThink.DataAccess.Repositories.Courses;
+
+public class CourseReqRepository : BaseRepository, ICourseReqRepository
+{
+    public ValueTask<long> CountAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> CreateAsync(CourseRequirments model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DeleteAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<CourseRequirments>> GetAllAsync(PaginationParams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseRequirments> GetByIdAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseRequirments> GetByNameAsync(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<(long ItemsCount, IEnumerable<CourseRequirments>)> SearchAsync(string search, PaginationParams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> UpdateAsync(CourseRequirments model)
+    {
+        throw new NotImplementedException();
+    }
+}
