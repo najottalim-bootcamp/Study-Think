2c6e494c8e2809eccc62f6f7fac69095bde497cc
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-04 14:57:30 +0500

    add AutoMapper in DataAccess

--- src/StudyThink.Api/Configurations/Layers/DataAccessConfiguration.cs
+++ src/StudyThink.Api/Configurations/Layers/DataAccessConfiguration.cs
@@ -4,6 +4,9 @@ public static class DataAccessConfiguration
 {
     public static void ConfigureDataAccess(this WebApplicationBuilder builder)
     {
+        // AutoMapping
+        builder.Services.AddAutoMapper(typeof(MapperConfiguration));
+
         // Database Configurations field
 
         //builder.Services.AddScoped<>();
