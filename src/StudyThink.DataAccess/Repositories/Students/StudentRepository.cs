be73bf217dcee3ea10b75c79609d1f449f820ced
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-05 20:35:35 +0500

    fix

--- src/StudyThink.DataAccess/Repositories/Students/StudentRepository.cs
+++ src/StudyThink.DataAccess/Repositories/Students/StudentRepository.cs
@@ -6,7 +6,7 @@ using StudyThink.Domain.Enums;
 
 namespace StudyThink.DataAccess.Repositories.Students;
 
-public class StudentRepository : BaseRepository, IStudentRepository
+public class StudentRepository : BaseRepository2, IStudentRepository
 {
     public async ValueTask<long> CountAsync()
     {
@@ -29,70 +29,19 @@ public class StudentRepository : BaseRepository, IStudentRepository
         }
     }
 
-    public async ValueTask<bool> CreateAsync(Student model)
+    public ValueTask<bool> CreateAsync(Student model)
     {
-        try
-        {
-            await _connection.OpenAsync();
-
-            string query = "INSERT INTO Students(FirstName, LasntName, DateOfBith, UserName, Password, Email, PhoneNumber, Gender, CreatedAt, UpdatedAt, DeletedAt, ImagePath) " +
-                "VALUES (@FirstName, @LasntName, @DateOfBith, @UserName, @Password, @Email, @PhoneNumber, @Gender, @CreatedAt, @UpdatedAt, @DeletedAt, @ImagePath)";
-
-            var result = await _connection.ExecuteAsync(query, model);
-
-            return result > 0;
-        }
-        catch
-        {
-            return false;
-        }
-        finally
-        {
-            await _connection.CloseAsync();
-        }
+        throw new NotImplementedException();
     }
 
-    public async ValueTask<bool> DeleteAsync(long Id)
+    public ValueTask<bool> DeleteAsync(long Id)
     {
-
-        try
-        {
-            await _connection.OpenAsync();
-            string query = $"DELETE FROM Students WHERE Id={Id}";
-            var result = await _connection.ExecuteAsync(query);
-            return result > 0;
-        }
-        catch (Exception)
-        {
-
-            return false;
-        }
-        finally
-        {
-            await _connection.CloseAsync();
-        }
+        throw new NotImplementedException();
     }
 
-    public async ValueTask<IEnumerable<Student>> GetAllAsync(PaginationParams @params)
+    public ValueTask<IEnumerable<Student>> GetAllAsync(PaginationParams @params)
     {
-        try
-        {
-            await _connection.OpenAsync();
-            string query = $"SELECT * FROM Students order by Id desc " +
-               $"offset {@params.GetSkipCount()} limit {@params.PageSize}";
-
-            IEnumerable<Student>? students = await _connection.ExecuteScalarAsync<IEnumerable<Student>>(query, @params);
-
-            return students;
-        }
-        catch (Exception)
-        {
-            return Enumerable.Empty<Student>();
-        }
-        finally
-        {
-            await _connection.CloseAsync();
-        }
+        throw new NotImplementedException();
     }
 
     public ValueTask<Student> GetByEmailAsync(string email)
@@ -105,26 +54,9 @@ public class StudentRepository : BaseRepository, IStudentRepository
         throw new NotImplementedException();
     }
 
-    public async ValueTask<Student> GetByIdAsync(long Id)
+    public ValueTask<Student> GetByIdAsync(long Id)
     {
-        try
-        {
-            await _connection.OpenAsync();
-            string query = $"SELECT * FROM Students " +
-                $"WHERE Id = {Id}";
-            Student students = await _connection.ExecuteScalarAsync<Student>(query);
-            return students;
-
-        }
-        catch (Exception)
-        {
-            return new Student();
-
-        }
-        finally
-        {
-            await _connection.CloseAsync();
-        }
+        throw new NotImplementedException();
     }
 
     public ValueTask<IEnumerable<Student>> GetByPhoneNumberAsync(string phoneNumber)
@@ -142,26 +74,9 @@ public class StudentRepository : BaseRepository, IStudentRepository
         throw new NotImplementedException();
     }
 
-    public async ValueTask<bool> UpdateAsync(Student model)
+    public ValueTask<bool> UpdateAsync(Student model)
     {
-        try
-        {
-            await _connection.OpenAsync();
-            string query = $"Update Students SET FirstName='{model.FirstName}',LastName='{model.LastName}',DateOfBirth={model.DateOfBirth},UserName='{model.Username}'," +
-                $"Password='{model.Password}',Email='{model.Email}',PhoneNumber='{model.PhoneNumber}',Gender='{model.Gender}'," +
-                $"CreatedAt={model.CreatedAt},UpdatedAt={model.UpdatedAt},ImagePath='{model.ImagePath}'";
-            var result = await _connection.ExecuteAsync(query, model);
-            return result > 0;
-        }
-        catch (Exception)
-        {
-
-            return false;
-        }
-        finally
-        {
-            await _connection.CloseAsync();
-        }
+        throw new NotImplementedException();
     }
 
     public Task<bool> UpdateImageAsync(long studentId, string imagePath)
