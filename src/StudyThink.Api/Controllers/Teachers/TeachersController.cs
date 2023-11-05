254d147365323ebe734e398040e0d0df5dfa3d9d
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-05 17:26:34 +0500

    TeacherController Created

---
+++ src/StudyThink.Api/Controllers/Teachers/TeachersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudyThink.Domain.Entities.Teachers;
+using StudyThink.Service.Interfaces.Teachers;
+
+namespace StudyThink.Api.Controllers.Teachers
+{
+    [ApiController]
+    [Route("api/[countroller]/[api]")]
+    public class TeachersController : ControllerBase
+    {
+        private readonly ITeacherService _teacherService;
+        public TeachersController(ITeacherService teacherService)
+        {
+            _teacherService = teacherService;
+        }
+
+        public async ValueTask<IActionResult> GetById(long id)
+        {
+            Teacher teacher = await _teacherService.GetByIdAsync(id);
+
+            return Ok(teacher);
+        }
+    }
+}
