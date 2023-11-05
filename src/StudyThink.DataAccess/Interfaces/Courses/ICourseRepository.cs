b77a35c77012d4818e6b7b5c4df83ca2bb37bbc5
Abdualim Normurodov <abdualimnormurodov1@gmail.com>
2023-11-05 19:01:45 +0500

    fix

--- src/StudyThink.DataAccess/Interfaces/Courses/ICourseRepository.cs
+++ src/StudyThink.DataAccess/Interfaces/Courses/ICourseRepository.cs
@@ -1,15 +1,14 @@
-﻿using StudyThink.DataAccess.Common;
-using StudyThink.DataAccess.Interfaces;
+﻿using StudyThink.DataAccess.Interfaces;
+using StudyThink.DataAccess.Utils;
+using StudyThink.Domain.Entities.Course;
 using StudyThink.Domain.Entities.Courses;
+using System.ComponentModel;
 
 namespace StudyThink.Service.Interfaces.Courses;
 
-public interface ICourseRepository : IRepository<Course>,
-    IGetAll<Course>, ISearchable<Course>
+public  interface ICourseRepository:IRepository<Course>
 {
-
-    ValueTask<IEnumerable<Course>> GetByNameAsync(string name);
-
-    ValueTask<bool> UpdateImageAsync(long categoryId, string imagePath);
+    ValueTask<IEnumerable<Course>> GetAllAsync(PaginationParams @params);
+    ValueTask<IEnumerable<Course>> GetByNameAsync (string name);
 
 }
