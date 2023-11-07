d7b78f7e12ef5acda6bad580912e834692fa9484
Abdualim Normurodov <abdualimnormurodov1@gmail.com>
2023-11-07 14:35:41 +0500

    Add PDController IPDService

---
+++ src/StudyThink.Api/Controllers/PaymentDetails/PaymentDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudyThink.Service.Interfaces.Payments;
+
+namespace StudyThink.Api.Controllers.PaymentDetails;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class PaymentDetailsController : ControllerBase
+{
+    private readonly IPaymentDetailsService _paymentDetailsService;
+    private readonly int _maxPageSize = 30;
+}
