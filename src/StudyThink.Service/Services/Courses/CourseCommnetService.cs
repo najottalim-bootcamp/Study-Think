4cda990a4d91f3a496f95590c40b5d5f420fe309
nodirbek004 <allanazarovnodirshax@gmail.com>
2023-11-07 11:54:44 +0500

    Course Comment Service Ready

--- src/StudyThink.Service/Services/Courses/CourseCommnetService.cs
+++ src/StudyThink.Service/Services/Courses/CourseCommnetService.cs
@@ -1,5 +1,8 @@
-﻿using StudyThink.DataAccess.Utils;
+﻿using AutoMapper;
+using StudyThink.DataAccess.Utils;
 using StudyThink.Domain.Entities.Course;
+using StudyThink.Domain.Exceptions.Courses.CourseComments;
+using StudyThink.Service.Common.Helpers;
 using StudyThink.Service.DTOs.Courses.CourseComment;
 using StudyThink.Service.Interfaces.Common;
 using StudyThink.Service.Interfaces.Courses;
@@ -10,27 +13,42 @@ public class CourseCommnetService : ICourseCommentService
 {
     private readonly ICourseCommentRepository _repository;
     private readonly IFileService _fileService;
+    private readonly IMapper _mapper;
 
     public CourseCommnetService(ICourseCommentRepository repository,
-        IFileService fileService)
+        IFileService fileService,IMapper mapper)
     {
         this._repository = repository;
         this._fileService = fileService;
+        this._mapper = mapper;
     }
 
-    public ValueTask<long> CountAsync()
+    public async ValueTask<long> CountAsync()
+        =>await _repository.CountAsync();
+
+    
+
+    public async ValueTask<bool> CreateAsync(CourseCommentCreationDto model)
     {
-        throw new NotImplementedException();
+        var courseComment= _mapper.Map<CourseComment>(model);
+
+        courseComment.CreatedAt = TimeHelper.GetDateTime();
+        courseComment.UpdatedAt = TimeHelper.GetDateTime();
+        
+        var result = await _repository.CreateAsync(courseComment);
+        return result;
+
     }
 
-    public ValueTask<bool> CreateAsync(CourseCommentCreationDto model)
+    public async ValueTask<bool> DeleteAsync(long id)
     {
-        throw new NotImplementedException();
-    }
-
-    public ValueTask<bool> DeleteAsync(long id)
-    {
-        throw new NotImplementedException();
+        var existCourseComment=await _repository.GetByIdAsync(id);
+        if (existCourseComment is null)
+        {
+            throw new CourseCommentsAlreadyExistsException();
+        }
+        var result = await _repository.DeleteAsync(id);
+        return result;
     }
 
     public ValueTask<bool> DeleteRangeAsync(List<long> CourseCommentIds)
@@ -38,18 +56,36 @@ public class CourseCommnetService : ICourseCommentService
         throw new NotImplementedException();
     }
 
-    public ValueTask<IEnumerable<CourseComment>> GetAllAsync(PaginationParams @params)
+    public async ValueTask<IEnumerable<CourseComment>> GetAllAsync(PaginationParams @params)
     {
-        throw new NotImplementedException();
+        var result = await _repository.GetAllAsync(@params);
+        if (result is null)
+        {
+            throw new CourseCommentsNotFoundException();
+        }
+        return result;
     }
 
-    public ValueTask<CourseComment> GetById(long id)
+    public async ValueTask<CourseComment> GetById(long id)
     {
-        throw new NotImplementedException();
+        var result =await _repository.GetByIdAsync(id);
+        if(result is null)
+        {
+            throw new CourseCommentsNotFoundException();
+        }
+        return result;
     }
 
-    public ValueTask<bool> UpdateAsync(CourseCommentUpdateDto model)
+    public async ValueTask<bool> UpdateAsync(CourseCommentUpdateDto model)
     {
-        throw new NotImplementedException();
+        var existCpurseComment = await _repository.GetByIdAsync(model.Id);
+        if(existCpurseComment is null)
+        {
+            throw new CourseCommentsNotFoundException();
+        }
+        CourseComment courseComment =_mapper.Map<CourseComment>(existCpurseComment);
+        courseComment.UpdatedAt = TimeHelper.GetDateTime();
+        var result = await _repository.UpdateAsync(courseComment);
+        return result;
     }
 }
