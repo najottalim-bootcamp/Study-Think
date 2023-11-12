031bb1b7bb528863c4971f394b2c3da8b831ae1c
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-12 21:50:00 +0500

    add CreateAsync method in adminService

--- src/StudyThink.Service/Services/Admins/AdminService.cs
+++ src/StudyThink.Service/Services/Admins/AdminService.cs
@@ -1,6 +1,12 @@
-﻿using StudyThink.DataAccess.Interfaces.Admins;
+﻿using AutoMapper;
+using StudyThink.DataAccess.Interfaces.Admins;
 using StudyThink.DataAccess.Utils;
 using StudyThink.Domain.Entities.Admins;
+using StudyThink.Domain.Entities.Students;
+using StudyThink.Domain.Exceptions.Admin;
+using StudyThink.Domain.Exceptions.Student;
+using StudyThink.Service.Common.Hasher;
+using StudyThink.Service.Common.Helpers;
 using StudyThink.Service.DTOs.Admin;
 using StudyThink.Service.Interfaces.Admins;
 using StudyThink.Service.Interfaces.Common;
@@ -11,20 +17,37 @@ public class AdminService : IAdminService
 {
     private readonly IAdminRepository _repository;
     private readonly IFileService _fileService;
+    private readonly IMapper _mapper;
 
     public AdminService(IAdminRepository adminRepository,
-        IFileService fileService)
+        IFileService fileService,
+        IMapper mapper)
     {
         this._repository = adminRepository;
         this._fileService = fileService;
+        this._mapper = mapper;
     }
 
     public async ValueTask<long> CountAsync()
         => await _repository.CountAsync();
 
-    public ValueTask<bool> CreateAsync(AdminCreationDto model)
+    public async ValueTask<bool> CreateAsync(AdminCreationDto model)
     {
-        throw new NotImplementedException();
+        var exits = await _repository.GetByEmailAsync(model.Email);
+
+        if (exits is not null)
+            throw new AdminAlreadyExistsException();
+
+        var admin = _mapper.Map<Admin>(model);
+
+        admin.Password = Hash512.GenerateHash512(admin.Password);
+
+        admin.CreatedAt = TimeHelper.GetDateTime();
+        admin.UpdatedAt = TimeHelper.GetDateTime();
+
+        var result = await _repository.CreateAsync(admin);
+
+        return result;
     }
 
     public ValueTask<bool> DeleteAsync(long Id)
